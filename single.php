0767e066f5ac1f809c7d2fafb969a5d87f31fb0e
mushraf377 <freelancer.musharaf@gmail.com>
2026-04-09 01:30:06 +0600

    Related blog added.

--- single.php
+++ single.php
@@ -25,7 +25,7 @@ if ($drillcorp->is_drillcorp_core_active()) {
                         <a href="<?php echo esc_url(home_url('/')); ?>"><?php echo esc_html__('Home', 'drillcorp'); ?></a>
                         <p>Blog Details</p>
                     </div>
-                    <h1 class="blog-main-title"><?php echo get_the_title(get_the_ID()); ?></h1>        
+                    <h1 class="blog-main-title"><?php echo get_the_title(get_the_ID()); ?></h1>
                 </div>
             </div>
 
@@ -48,32 +48,32 @@ if ($drillcorp->is_drillcorp_core_active()) {
                     <div class="<?php echo esc_attr($page_layout_meta['content_column_class']); ?>">
                         <div class="blog-left-content-wrap">
                             <?php
-                                the_content();
+                            the_content();
                             ?>
                         </div>
                     </div>
                     <?php if ($page_layout_meta['sidebar_enable']): ?>
-                        
-                            <div class="<?php echo esc_attr($page_layout_meta['sidebar_column_class']); ?>">
-                                <div class="blog-sidebar-area">    
-                                    <?php get_sidebar(); ?>
-                                    <div class="contact-information">
-                                        <a href="">Contact US <?php echo drillcorp_get_svg_icon('down_angle'); ?></a>
-                                        <div class="contact-information-wrapper">
-                                            <img class="contact-info-bg" src="<?php echo get_template_directory_uri(); ?>/assets/img/contact-info-bg.png" alt="">
-                                            <div class="contact-info-content">
-                                                <img  src="<?php echo get_template_directory_uri(); ?>/assets/img/contact-info-thumb.png" alt="" class="contact-person-thumb">
-                                                <h3 class="contact-info-name"><?php echo esc_html__('Tansu Ozerkan', 'drillcorp'); ?></h3>
-                                                <p class="contact-info-designation"><?php echo esc_html__('Web Developer', 'drillcorp'); ?></p>
-                                                <p class="contact-info-location"><?php echo esc_html__('Location', 'drillcorp'); ?></p>
-                                                <div class="contact-info-social">
-                                                    <a href=""><?php echo drillcorp_get_svg_icon('linkedin'); ?></a>
-                                                    <a href=""><?php echo drillcorp_get_svg_icon('twitter'); ?></a>                                                 
-                                                </div>
+
+                        <div class="<?php echo esc_attr($page_layout_meta['sidebar_column_class']); ?>">
+                            <div class="blog-sidebar-area">
+                                <?php get_sidebar(); ?>
+                                <div class="contact-information">
+                                    <a href="" class="contact-us-link">Contact US <?php echo drillcorp_get_svg_icon('down_angle'); ?></a>
+                                    <div class="contact-information-wrapper">
+                                        <img class="contact-info-bg" src="<?php echo get_template_directory_uri(); ?>/assets/img/contact-info-bg.png" alt="">
+                                        <div class="contact-info-content">
+                                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/contact-info-thumb.png" alt="" class="contact-person-thumb">
+                                            <h3 class="contact-info-name"><?php echo esc_html__('Tansu Ozerkan', 'drillcorp'); ?></h3>
+                                            <p class="contact-info-designation"><?php echo esc_html__('Web Developer', 'drillcorp'); ?></p>
+                                            <p class="contact-info-location"><?php echo esc_html__('Location', 'drillcorp'); ?></p>
+                                            <div class="contact-info-social">
+                                                <a href=""><?php echo drillcorp_get_svg_icon('linkedin'); ?></a>
+                                                <a href=""><?php echo drillcorp_get_svg_icon('twitter'); ?></a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
                         </div>
                     <?php endif; ?>
                 </div>
@@ -81,5 +81,63 @@ if ($drillcorp->is_drillcorp_core_active()) {
         </div>
     </main><!-- #main -->
 </div><!-- #primary -->
+
+<section class="related-blog-area">
+    <div class="container">
+        <div class="section-heading">
+            <h2>More Industry Insights</h2>
+            <button class=primary-btn>View All Insights</button>
+        </div>
+
+        <?php
+
+        $args = [
+            'post_type'           => 'post',
+            'posts_per_page'      => 3,
+            'orderby'             => 'date',
+            'order'               => 'DESC',
+            'post_status'         => 'publish',
+        ];
+
+        $query = new \WP_Query($args);
+
+        if (! $query->have_posts()) {
+            echo '<p>' . esc_html__('No blog posts found.', 'drillcorp-core') . '</p>';
+            return;
+        }
+
+
+        ?>
+        <div class="blog-list related-blog-list">
+            <?php while ($query->have_posts()) : $query->the_post(); ?>
+                <article class="blog-list-item">
+                    <a class="blog-list-item-thumb" href="<?php echo esc_url(get_permalink()); ?>">
+                        <?php the_post_thumbnail('large'); ?>
+                    </a>
+                    <div class="blog-list-item-content">
+                        <div>
+                            <div class="blog-list-item-cats">
+                                <div class="blog-list-item-cat-dot"></div>
+                                <?php echo wp_kses_post(get_the_category_list(', ')); ?>
+                            </div>
+                            <h3 class="blog-list-item-title">
+                                <a href="<?php echo esc_url(get_permalink()); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h3>
+                        </div>
+                        <div class="blog-list-item-meta">
+                            <div class="blog-list-item-date">
+                                <?php echo get_the_date(); ?>
+                            </div>
+                            <div class="blog-list-meta-dot"></div>
+                            <div class="blog-read-time"><?php echo drillcorp()->get_reading_time(get_the_ID()); ?> Min Read</div>
+                        </div>
+                    </div>
+                </article>
+            <?php endwhile; ?>
+        </div>
+    </div>
+</section>
 <?php
 get_footer();
